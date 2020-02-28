889fd24881038f10d72ff5b5f601464f54750188
Simeon Raykov <makenr3@gmail.com>
2020-02-28 09:27:29 +0200

    Add files via upload

---
+++ copy-file.bat
@@ -0,0 +1,4 @@
+echo off
+copy C:\Users\%USERNAME%\Desktop\Bat-Files\copy.txt C:\Users\%USERNAME%\Desktop
+echo %USERNAME%
+pause
---
+++ move-file.bat
@@ -0,0 +1,3 @@
+echo off
+move C:\Users\%USERNAME%\Desktop\Bat-Files\copy.txt C:\Users\%USERNAME%\Desktop
+pause
